ac3df96119190d848455204cb712dca1dd3bd3a2
Bhuvaneshvar <bhuvaneshvar8@gmail.com>
2023-07-10 20:26:11 +0530

    Initial commit

---
+++ src/main/java/com/bhvan/breddit/BRedditApplication.java
@@ -0,0 +1,13 @@
+package com.bhvan.breddit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BRedditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BRedditApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bhvan/breddit/BRedditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhvan.breddit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BRedditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
